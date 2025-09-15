fd80997bf981d377564161a3bff371516d7bb24f
Yonghyeon Kim <yonghyeon0223@gmail.com>
2025-09-15 12:10:59 +0900

    port scan detectin analsis

--- src/graph.py
+++ src/graph.py
@@ -1,4 +1,6 @@
 import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
+import pandas as pd
 
 
 def plot_protocol_distribution(protocol_counter, save_path):
@@ -20,4 +22,27 @@ def plot_protocol_distribution(protocol_counter, save_path):
             " " + str(count),
             va="center",
         )
-    plt.savefig(save_path, dpi=600, bbox_inches="tight")
+    plt.savefig(save_path)
+
+
+def plot_port_usage_over_time(df: pd.DataFrame, save_path):
+    df["time_bin"] = df["time"].dt.floor("1T")
+    unique_counts = df.groupby("time_bin")["port"].nunique().reset_index()
+    print(unique_counts)
+
+    plt.figure(figsize=(12, 8))
+    plt.plot(
+        unique_counts["time_bin"],
+        unique_counts["port"],
+        marker="o",
+        color="steelblue",
+        linewidth=2,
+    )
+    plt.xlabel("Time")
+    plt.ylabel("Unique Ports Contacted")
+    plt.title("Unique Ports Contacted per 1-Minute Window")
+    plt.gca().xaxis.set_major_formatter(mdates.DateFormatter("%H:%M"))
+    plt.grid(True, linestyle="--", alpha=0.5)
+    plt.tight_layout()
+    plt.show()
+    plt.savefig(save_path)
--- src/main.py
+++ src/main.py
@@ -1,17 +1,19 @@
 import analyser
 import graph
 from pathlib import Path
+import time
 
 
 def analyse_example_pcap_file(logname: str):
     LOGPATH = f"logs/{logname}.pcap"
 
-    # A1 = "protocol_distribution"
-    # dist = analyser.compute_protocol_distribution(LOGPATH)
-    # graph.plot_protocol_distribution(dist, f"report/{logname}_{A1}.png")
+    A1 = "protocol_distribution"
+    dist = analyser.compute_protocol_distribution(LOGPATH)
+    graph.plot_protocol_distribution(dist, f"report/{logname}_{A1}.png")
 
     A2 = "port_usage_over_time"
     port_usage = analyser.compute_port_usage(LOGPATH)
+    graph.plot_port_usage_over_time(port_usage, f"report/{logname}_{A2}.png")
 
 
 if __name__ == "__main__":
